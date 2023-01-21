106dd3fca6ae674e6af0f4b6da1b5f0f5d831acd
Arvind Pandey <arvind3157@gmail.com>
2023-01-21 16:36:13 +0530

    [add] openai

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -1,7 +1,22 @@
+require "ruby/openai"
+
 class TextController < ApplicationController
     def process_text
-        @input_text = params[:input_text]
-        @output_text = "Your input text is: #{@input_text}"
+        openai = OpenAI::Client.new(access_token:ENV["OPENAI_API_KEY"])
+        @input_text = params[:input_text] || ""
+        response = openai.completions(
+            parameters: {
+                model: "text-davinci-002",
+                prompt: @input_text,
+                temperature: 0.5,
+                max_tokens: 500,
+                stop: "."
+                }
+                )
+        if @input_text.empty?
+            @output_text = "Enter the prompt!!"
+        else
+            @output_text = response["choices"][0]["text"]
+        end
     end
-
 end
