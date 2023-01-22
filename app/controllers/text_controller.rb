a0dae7f04a127cf81de527d69866bbbeb616092e
Arvind Pandey <arvind3157@gmail.com>
2023-01-22 17:41:41 +0530

    moved the process text logic to a helper method

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -1,21 +1,5 @@
-require "ruby/openai"
-
 class TextController < ApplicationController
     def process_text
-        openai = OpenAI::Client.new(access_token:ENV["OPENAI_API_KEY"])
-        @input_text = params[:input_text] || ""
-        response = openai.completions(
-            parameters: {
-                model: "text-davinci-003",
-                prompt: @input_text,
-                temperature: 0.7,
-                max_tokens: 500,
-                }
-                )
-        if @input_text.empty?
-            @output_text = "Enter the prompt!"
-        else
-            @output_text = response["choices"][0]["text"]
-        end
+        @output_text = helpers.process_text(params);
     end
 end
--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -1,5 +1,21 @@
+require "ruby/openai"
+
 module TextHelper
-    def say_hello(name)
-       puts "Hello, #{name}"
+    def process_text(params)
+        openai = OpenAI::Client.new(access_token:ENV["OPENAI_API_KEY"])
+        @input_text = params[:input_text] || ""
+        response = openai.completions(
+            parameters: {
+                model: "text-davinci-003",
+                prompt: @input_text,
+                temperature: 0.7,
+                max_tokens: 500,
+                }
+                )
+        if @input_text.empty?
+            return "Enter the prompt!"
+        else
+            return response["choices"][0]["text"]
+        end
     end
 end
